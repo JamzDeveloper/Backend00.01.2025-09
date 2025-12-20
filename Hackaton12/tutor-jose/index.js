4f820b9cc99a88a19bc953b33f10549249c16718
jamz <jamzdeveloper@gmail.com>
2025-12-20 16:32:47 -0500

    feat: added post create list

--- Hackaton12/tutor-jose/index.js
+++ Hackaton12/tutor-jose/index.js
@@ -12,6 +12,16 @@ Server.on("request", (request, res) => {
   console.log("path", path);
   console.log("method", request.method);
 
+  if (request.method === "GET" && path == "/") {
+    res.writeHead(200, { "Content-Type": "application/json" });
+    return res.end(
+      JSON.stringify({
+        message: "Api listening in port 4000",
+        data: {},
+      })
+    );
+  }
+
   if (request.method === "GET" && path == "/api/lista") {
     res.writeHead(200, { "Content-Type": "application/json" });
     return res.end(
@@ -20,6 +30,53 @@ Server.on("request", (request, res) => {
         data: listSales,
       })
     );
+  }
+
+  if (request.method === "POST" && path == "/api/lista") {
+    try {
+      let body = "";
+
+      request.on("data", (chunk) => {
+        body += chunk.toString();
+      });
+
+      return request.on("end", () => {
+        const { name, description, date, esCompletado } = JSON.parse(body);
+        
+        if (!name || !description || !date || !esCompletado) {
+          res.writeHead(400, { "Content-Type": "application/json" });
+          return res.end(
+            JSON.stringify({
+              message: "faltan campos",
+              data: {},
+            })
+          );
+        }
+
+        const data = {
+          name,
+          description,
+          date,
+          esCompletado,
+        };
+        listSales.push(data);
+        res.writeHead(200, { "Content-Type": "application/json" });
+        return res.end(
+          JSON.stringify({
+            message: "created success",
+            data: data,
+          })
+        );
+      });
+    } catch (err) {
+      res.writeHead(500, { "Content-Type": "application/json" });
+      return res.end(
+        JSON.stringify({
+          message: "Internal server error",
+          data: {},
+        })
+      );
+    }
   }
 
   res.writeHead(404, { "Content-Type": "application/json" });
